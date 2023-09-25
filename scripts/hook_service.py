d321bea02cb4df33ce59a90e7df822b683f35bfb
enco77 <tursunbaevsahboz@gmail.com>
2023-09-25 14:32:27 +0500

    saved from amr camel 500 last

--- scripts/hook_service.py
+++ scripts/hook_service.py
@@ -2,18 +2,31 @@
 import rospy
 from ethernet_remote_io_module.msg import WriteCoil
 from std_srvs.srv import SetBool, SetBoolResponse
-from Light_control import LightControl
 
 
 class HookService:
     def __init__(self):        
-        self._as = rospy.Service('/camel_amr_1000_001/common/hooks_ctrl', SetBool , handler=self.hook_command)
-        self.pub = rospy.Publisher('/camel_amr_1000_001/common/write_coil', WriteCoil, queue_size=1)
+        self._as = rospy.Service('hooks_ctrl', SetBool , handler=self.hook_command)
+        self.pub = rospy.Publisher('common/write_coil', WriteCoil, queue_size=1)
         self.rate = rospy.Rate(10)
         self.pub_msg = WriteCoil()
-        self.lc = LightControl()
-
     
+    def hook_up(self):
+        """
+            Hook up
+        """
+        self.pub_msg.address = 0
+        self.pub_msg.value = True
+        self.pub.publish(self.pub_msg)
+    
+    def hook_down(self):
+        """
+            Hook down
+        """
+        self.pub_msg.address = 0
+        self.pub_msg.value = False
+        self.pub.publish(self.pub_msg)
+
     def hook_command(self, command : SetBool) -> SetBoolResponse:
         """ 
         Service handler recives hook command in SetBool format
@@ -28,15 +41,14 @@ class HookService:
         
         # If request command is True Hooks will be UP!
         if (request_command == True):
-            self.lc.hook_up()
+            self.hook_up()
             return SetBoolResponse(True, "hooks are up")
         
         # If request command is False Hooks will be Down!
         else:
-            self.lc.hook_down()
+            self.hook_down()
             return SetBoolResponse(True, "hooks are down")
 
-        
 
 if __name__ == "__main__":
     rospy.init_node('Hook_Service', anonymous=True)   
